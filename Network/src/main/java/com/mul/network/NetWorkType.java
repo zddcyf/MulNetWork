b177abd21a4c7b4bf7795180f61df1e1297ac0cc
zdd <zhangdcyf@qq.com>
2020-11-20 14:28:21 +0800

    *)修改版本号，以及网络异常代表的数据

--- Network/src/main/java/com/mul/network/NetWorkType.java
+++ Network/src/main/java/com/mul/network/NetWorkType.java
@@ -21,12 +21,12 @@ import androidx.annotation.IntDef;
         , NetWorkType.NW_WIFI
         , NetWorkType.NW_ETHERNET
         , NetWorkType.NW_UNKNOWN})
-@interface NetWorkType {
+public @interface NetWorkType {
+    int NW_UNKNOWN = -1; // 无网络
     int NW_2G = 2; // 2G网
     int NW_3G = 3; // 3G网
     int NW_4G = 4; // 4G网
     int NW_5G = 5; // 5G网
     int NW_WIFI = 6; // wifi网
     int NW_ETHERNET = 7; // 以太网
-    int NW_UNKNOWN = 8; // 无网络
 }
